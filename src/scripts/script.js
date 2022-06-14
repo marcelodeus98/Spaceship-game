62bd3d6256f1458b86074ff106d497140c2e5ef0
marcelodeus98 <marcelodeus98@gmail.com>
2022-06-14 00:53:31 -0300

    config

---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+let start = () => {
+    $('#init').hide();
+
+    $('#background-game').append("<div id='player'></div>");
+    $('#background-game').append("<div id='enemy1'></div>");
+    $('#background-game').append("<div id='enemy2'></div>");
+    $('#background-game').append("<div id='friend'></div>");
+
+}
